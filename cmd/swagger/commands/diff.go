9d8352675b63a121ce516c116cfe3ec57a34ccd9
Mateusz Bilski <mateusz.bilski@gmail.com>
2022-10-20 10:52:15 +0200

    Change root

--- cmd/swagger/commands/diff.go
+++ cmd/swagger/commands/diff.go
@@ -9,7 +9,7 @@ import (
 	"os"
 
 	"github.com/go-openapi/loads"
-	"github.com/go-swagger/go-swagger/cmd/swagger/commands/diff"
+	"github.com/cloudentity/go-swagger/cmd/swagger/commands/diff"
 )
 
 // JSONFormat for json
